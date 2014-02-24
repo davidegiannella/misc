2ff5cb301f628b747061a8176e1545056c8d7198
Davide Giannella <giannella.davide@gmail.com>
2014-02-24 17:57:55 +0100

    added parametrisation of runtime and commit

--- oaky-stuff/atomic-benchmark.sh
+++ oaky-stuff/atomic-benchmark.sh
@@ -4,19 +4,24 @@ set -e
 # shell scripts
 #
 # Usage:
-#   atomic-benchmark.sh <oak-jar> <tests> <log-file> [pre-added-nodes] [nodes-per-iteration]
+#   atomic-benchmark.sh <oak-jar> <tests> <log-file> [pre-added-nodes] [nodes-per-iteration] [runtime] [warmup]
 # 
 #   [pre-added-nodes] and [nodes-per-iteration] are optional and mean respectively a number of nodes to be added 
 #       before running the test and the number of nodes to be inserted during the test. Assuming the test will
-#       cope with them.       
+#       cope with them.
+#
+#   [runtime] and [warmup] are optional as well and will provide the corresponding attributes to the
+#       benchmark framework
+#
+#   !!! IF YOU WANT TO PROVIDE 'WARMUP', FOR EXAMPLE, YOU HAVE TO PROVIDE THE WHOLE SET OF ARGUMENTS !!!       
 #
 # for example
 #
 # ./atomic-benchmark.sh oak-run-*.jar "OrderedIndexInsertNoIndexTest OrderedIndexInsertStandardPropertyTest OrderedIndexInsertOrderedPropertyTest"  /var/log/foobar.log
 
 FIXTURES=Oak-Tar
-RUNTIME=15
-WARMUP=0
+RUNTIME=60
+WARMUP=30
 
 if [ -z "$1" -o -z "$2" -o -z "$3" ]
 then
@@ -38,6 +43,16 @@ NODES_PER_ITERATION=
 if [ ! -z "$5" ] 
 then
     NODES_PER_ITERATION="-DnodesPerIteration=${5}"
+fi
+
+if [ ! -z "${6}" ]
+then
+    RUNTIME=$6
+fi
+
+if [ ! -z "$7" ]
+then
+    WARMUP=$7
 fi
 
 java -Xmx2048m -Dprofile=false -Druntime=${RUNTIME} -Dwarmup=${WARMUP} \
