2596c6b7e71aabe78c1aaa99c71a8355d7948198
Davide Giannella <giannella.davide@gmail.com>
2014-04-10 11:11:06 +0100

    small refactoring

--- oaky-stuff/ut/src/test/java/org/apache/jackrabbit/oak/OrderedIndexAllTests.java
+++ oaky-stuff/ut/src/test/java/org/apache/jackrabbit/oak/OrderedIndexAllTests.java
@@ -1,3 +1,4 @@
+package org.apache.jackrabbit.oak;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
