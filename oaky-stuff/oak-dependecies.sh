03bcbbea3496a2f54db22a15320f9aab67665326
Davide Giannella <giannella.davide@gmail.com>
2017-03-21 14:14:18 +0000

    added mongo java driver version

--- oaky-stuff/oak-dependecies.sh
+++ oaky-stuff/oak-dependecies.sh
@@ -31,6 +31,7 @@ grep "lucene.version" ./${CHECKOUT_DIR}/oak-parent/pom.xml
 grep "<h2.version>" ./${CHECKOUT_DIR}/oak-parent/pom.xml
 commonsmath=`grep "math3" -A 2 ./${CHECKOUT_DIR}/oak-parent/pom.xml | sed 'N; s/.*<version>\([^<]*\).*/\1/g'`
 echo "    <commons.math>${commonsmath}<commons.math>"
+grep "<mongo.driver.version>" ./${CHECKOUT_DIR}/oak-parent/pom.xml
 
 echo "Deleting checkout dir"
 rm -rf ${CHECKOUT_DIR}
