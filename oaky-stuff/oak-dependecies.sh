7a7c532f7214989a14d7e04fbf67188479072262
Davide Giannella <giannella.davide@gmail.com>
2018-01-04 11:56:00 +0000

    Added tika version to the script

--- oaky-stuff/oak-dependecies.sh
+++ oaky-stuff/oak-dependecies.sh
@@ -32,6 +32,7 @@ grep "<h2.version>" ./${CHECKOUT_DIR}/oak-parent/pom.xml
 commonsmath=`grep "math3" -A 2 ./${CHECKOUT_DIR}/oak-parent/pom.xml | sed 'N; s/.*<version>\([^<]*\).*/\1/g'`
 echo "    <commons.math>${commonsmath}<commons.math>"
 grep "<mongo.driver.version>" ./${CHECKOUT_DIR}/oak-parent/pom.xml
+grep "<tika.version>" ./${CHECKOUT_DIR}/oak-parent/pom.xml
 
 echo "Deleting checkout dir"
 rm -rf ${CHECKOUT_DIR}
